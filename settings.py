cf014767229b5b0c0aaf95875c851dc859404051
JonathanReynoso <98475184+Jonathan6409@users.noreply.github.com>
2022-04-11 16:44:43 +0000

    Initial commit

---
+++ settings.py
@@ -0,0 +1,29 @@
+LEVEL_MAP = [
+'                                               ',
+'                                                         ',
+'                                                                           ',
+'       XXXX                                                                ',
+'   P                                                                       ',
+'XXXXX         XXX         XXX            XX   .                         X   ',
+' XXXX       XX                                             XXX          XXX  ',
+' XX    X  XXXX    XX  XX    XXX   XX   XXX   XXX      XXX  XXX   XXX  XXXXXXX',
+'       X  XXXX    XX  XXX        XX    XXX   XXX      XXX  XXX   XXX  XXXXXXX',
+'    XXXX  XXXXXX  XX  XXXX       XX    XXX   XXX      XXX  XXX   XXX  XXXXXXX',
+'XXXXXXXX  XXXXXX  XX  XXXX       XX    XXXXXXXXX      XXX  XXX   XXX  XXXXXXX']
+
+TILE_SIZE = 64
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+
+BG_COLOR = '#D6EAF8'
+PLAYER_COLOR = '#FF0000'
+TILE_COLOR = '#AED6F1'
+
+
+CAMERA_BORDERS = {
+	'left': 100,
+	'right': 200,
+	'top':100,
+	'bottom': 150
+}
